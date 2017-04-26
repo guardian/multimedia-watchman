c664e011d571456913a3f325a8198ad567732d8d
Andy Gallagher <andy.gallagher@theguardian.com>
2017-04-26 13:32:32 +0100

    setting up for circleci

--- buildrpm.sh
+++ buildrpm.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+function increment_release {
+    FILENAME=$1
+
+    RELEASEVER=$(grep '%define release' ${FILENAME} | awk -F ' ' '{print $3}')
+    if [ ${CIRCLE_BUILD_NUM} != "" ]; then
+        NEWVER=${CIRCLE_BUILD_NUM}
+    else
+        NEWVER=$(($RELEASEVER+1))
+    fi
+    echo Release version was ${RELEASEVER}, now is ${NEWVER}
+    cat ${FILENAME} | sed "s/\%define release .*/%define release ${NEWVER}/" > ${FILENAME}.new
+    mv ${FILENAME} ${FILENAME}.old
+    mv ${FILENAME}.new ${FILENAME}
+}
+
 BUILD_DIR=${HOME}/rpmbuild
 VERSION=`grep WATCHMAN_VERSION setup.py | cut -d '"' -f2`   #extract version number from WATCHMAN_VERSION in setup.py
 
@@ -16,6 +31,8 @@ mkdir -p ${BUILD_DIR}/SOURCES
 tar cv . --exclude "./.git" --exclude "./.idea" --exclude ".DS_Store" --transform "s,^\.,Watchman-${VERSION},"  | gzip > ${BUILD_DIR}/SOURCES/Watchman-${VERSION}.tar.gz
 
 mkdir -p /etc/supervisor/conf.d
+
+increment_release Watchman.spec
 
 rpmbuild -bb Watchman.spec
 
