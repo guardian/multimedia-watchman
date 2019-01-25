930f68df364db49863ae87318b82d8cd4448b959
Dave Allison <david.allison@theguardian.com>
2019-01-25 15:45:40 +0000

    Set the default log level to INFO

--- watcher/global_settings.py
+++ watcher/global_settings.py
@@ -4,5 +4,5 @@ import logging
 CONFIG_FILE="/etc/ffqueue-config.xml"
 LOGFORMAT = '%(asctime)-15s - [%(filename)s] %(threadName)s %(funcName)s: %(levelname)s - %(message)s'
 LOGFORMAT_RUNNER = '%(asctime)-15s - [%(filename)s] %(threadName)s %(funcName)s: %(levelname)s - (%(watchfolder)s) %(message)s'
-LOGLEVEL = logging.DEBUG
+LOGLEVEL = logging.INFO
 LOGFILE = "/var/log/watchman/watchman.log"
